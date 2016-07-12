9806e9aa53fe64408b6dd2ccb57f699fe435a601
Bernie <b4456609@gmail.com>
2016-07-12 12:39:06 +0800

    disable cors

--- src/main/java/ntou/bernie/easylearn/EasylearnMobileAPP.java
+++ src/main/java/ntou/bernie/easylearn/EasylearnMobileAPP.java
@@ -40,16 +40,16 @@ public class EasylearnMobileAPP extends Application<EasylearnAPPConfiguration> {
     public void run(EasylearnAPPConfiguration configuration, Environment environment) throws Exception {
         environment.getObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-        final FilterRegistration.Dynamic cors =
-                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
-
-        // Configure CORS parameters
-        cors.setInitParameter("allowedOrigins", "*");
-        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
-        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
-
-        // Add URL mapping
-        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+//        final FilterRegistration.Dynamic cors =
+//                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+//
+//        // Configure CORS parameters
+//        cors.setInitParameter("allowedOrigins", "*");
+//        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin,X-Auth-Token");
+//        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+//
+//        // Add URL mapping
+//        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
 
         final Client client = new JerseyClientBuilder().build();
         final Client client1 = new JerseyClientBuilder().build();
